ac2af268fe04344f111fa56bd97670a34b917c85
Irni Dwi Adryani <irnidwi16@gmail.com>
2024-06-21 12:02:58 +0700

    add navbar

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css"; // Import file CSS untuk styling
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const navbar = (
+  <div className="navbar">
+    <div className="left">
+      <h1>BOOTCAMP Batch 1: Experiment</h1>
+    </div>
+    <div className="right">
+      <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+      </nav>
+    </div>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const element = <h1>This is React</h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(navbar, document.getElementById("navbar"));
